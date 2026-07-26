e0035f860a5cbd0e6b590ba9b2115c61f0533a5d
Prajwal Karajange <prajwalkarajange0409@gmail.com>
2026-07-26 12:08:21 +0530

    Create 0242-valid-anagram2.java

---
+++ 0242-valid-anagram/0242-valid-anagram2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int fre[]=new int[26];
+
+        for(int i=0;i<s.length();i++){
+             fre[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            fre[t.charAt(i)-'a']--;
+        }
+
+        for(int i:fre){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+
+    
+        
+    }
+}
