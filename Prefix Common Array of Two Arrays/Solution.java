28e5eda5ccb7720c7938d52921abfa996cb239a3
Prajwal Karajange <prajwalkarajange0409@gmail.com>
2026-07-28 19:36:02 +0530

    Update Solution.java

--- Prefix Common Array of Two Arrays/Solution.java
+++ Prefix Common Array of Two Arrays/Solution.java
@@ -5,21 +5,25 @@ public class Solution {
         int[] A = { 1, 3, 2, 4 };
 
         int[] B = { 3, 1, 2, 4 };
+        int[] c = new int[A.length];
 
-        
-        for(int i=0;i<A.length;i++){
-            int count=0;
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<=i;k++){
-                    if(A[j]==B[k]){
+        for (int i = 0; i < A.length; i++) {
+            int count = 0;
+            for (int j = 0; j <= i; j++) {
+                for (int k = 0; k <= i; k++) {
+                    if (A[j] == B[k]) {
                         count++;
                     }
                 }
-                
+                c[i] = count;
 
             }
-            
-            System.out.print(count+" ");
+
+        }
+        
+        for (int i : c) {
+
+            System.out.print(i + " ");
         }
     }
 
