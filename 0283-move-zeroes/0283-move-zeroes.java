ed7e6e4d35e5e980408cc7992be8055389d17daf
Prajwal Karajange <prajwalkarajange0409@gmail.com>
2026-08-03 00:17:03 +0530

    Time: 3 ms (18.42%), Space: 47.9 MB (20.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                index++;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                if (left != right) {
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                }
+                left++;
             }
-
         }
-        while (index < nums.length) {
-            nums[index]=0;
-            index++;
-        }
-
     }
 }
