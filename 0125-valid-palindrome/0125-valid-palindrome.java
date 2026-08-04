836339c850973b86c64598dc5ed1a080413912e7
Prajwal Karajange <prajwalkarajange0409@gmail.com>
2026-08-04 12:33:00 +0530

    Time: 2 ms (99.32%), Space: 44.4 MB (64.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int low = 0;
+        int high = s.length() - 1;
+
+        while (low < high) {
+            while (low < high && !Character.isLetterOrDigit(s.charAt(low))) {
+                low++;
+            }
+
+            while (low < high && !Character.isLetterOrDigit(s.charAt(high))) {
+                high--;
+            }
+
+            if (Character.toLowerCase(s.charAt(low)) != Character.toLowerCase(s.charAt(high))) {
+                return false;
+            }
+
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
